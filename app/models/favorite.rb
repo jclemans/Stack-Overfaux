212c1877e70e8a429a3a0173920028c64511dd5c
Jeremy Clemans & Nolan Miller <student@epicodus.com>
2014-04-14 16:36:24 -0700

    question creater can add favorite answer to question. UI improvements/styling to favorite button.

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,5 @@
 class Favorite < ActiveRecord::Base
   belongs_to :question
   belongs_to :answer
+  validates_uniqueness_of :question_id
 end
--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -3,4 +3,5 @@ require 'spec_helper'
 describe Favorite do
  it { should belong_to :question }
  it { should belong_to :answer }
+ it { should validate_uniqueness_of :question_id }
 end
